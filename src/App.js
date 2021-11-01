f70b885b567e26c60000659db92cb9936cedb7b6
chang.ding <chang.ding@speer.io>
2021-11-01 15:22:41 -0400

    display data

--- src/App.js
+++ src/App.js
@@ -1,73 +1,73 @@
 import './App.css';
 import React, { useState } from 'react';
-const request = require('request');
+const woeidInfo = require('./utils/woeidInfo');
+const forecastWeather = require('./utils/forecastWeather');
 
 function App() {
-	const geocode = (address, callback) => {
-		const url =
-			'https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=' +
-			encodeURIComponent(address);
-		request({ url, json: true }, (error, { body } = {}) => {
+	const [ city, setCity ] = useState('');
+	const [ location, setLocation ] = useState('');
+	const [ airPressure, setAirPressure ] = useState('');
+	const [ currentDate, setCurrentDate ] = useState('');
+	const [ humidity, setHumidity ] = useState('');
+	const [ maxTemp, setMaxTemp ] = useState('');
+	const [ minTemp, setMinTemp ] = useState('');
+	const [ predictability, setPredictability ] = useState('');
+	const [ weatherState, setWeatherState ] = useState('');
+	const [ windDirection, setWindDirection ] = useState('');
+
+	const Result = () => (
+		<div>
+			<p>City Name {location}</p>
+			<p>Air Pressure {airPressure}</p>
+			<p>Date {currentDate}</p>
+			<p>Humidity {humidity}</p>
+			<p>Max Temp {maxTemp}</p>
+			<p>Min Temp {minTemp}</p>
+			<p>Predictability {predictability}</p>
+			<p>Weather Condition {weatherState}</p>
+			<p>Wind direction {windDirection}</p>
+		</div>
+	);
+
+	const getWeatherByCity = (evt) => {
+		evt.preventDefault();
+
+		woeidInfo(city, (error, { longitude, latitude, location, woeid } = {}) => {
 			if (error) {
-				callback('Unable to connect to server');
+				alert(error);
 			} else {
-        console.log(body);
-				callback(undefined, {
-					longitude: body[0]['latt_long'].split(',')[1],
-					latitude: body[0]['latt_long'].split(',')[0],
-					location: body[0]['title'],
-					woeid: body[0]['woeid']
+				forecastWeather(longitude, latitude, woeid, (error, forecastdata) => {
+					if (error) {
+						alert(error);
+					} else if (!forecastdata || forecastdata.length < 1) {
+						alert('no data available');
+					} else {
+						setLocation(location);
+						setAirPressure(forecastdata[0]['air_pressure']);
+						setCurrentDate(forecastdata[0]['applicable_date']);
+						setHumidity(forecastdata[0]['humidity']);
+						setMaxTemp(forecastdata[0]['max_temp']);
+						setMinTemp(forecastdata[0]['min_temp']);
+						setPredictability(forecastdata[0]['predictability']);
+						setWeatherState(forecastdata[0]['weather_state_name']);
+						setWindDirection(forecastdata[0]['wind_direction_compass']);
+					}
 				});
 			}
 		});
 	};
 
-	const forecast = (longitude, latitude, woeid, callback) => {
-		console.log('longitude: ' + longitude);
-		console.log('latitude" ' + latitude);
-		console.log('woeid" ' + woeid);
-		const url =
-			'https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/' + encodeURIComponent(woeid);
-		request({ url, json: true }, (error, { body } = {}) => {
-			if (error) {
-				callback(error);
-			} else if (body.error) {
-				callback(body.error);
-			} else {
-				callback(undefined, body['consolidated_weather']);
-			}
-		});
-	};
-
-	const [ city, setCity ] = useState('');
-
-	const handleSubmit = (evt) => {
-		evt.preventDefault();
-		// alert(`Submitting Name ${city}`);
-
-		geocode(city, (error, { longitude, latitude, location, woeid } = {}) => {
-			if (error) {
-				alert('error happened');
-			}
-
-			forecast(longitude, latitude, woeid, (error, forecastdata) => {
-				if (error) {
-					alert('error happened');
-				}
-				alert('Weather: ' + forecastdata[0]['weather_state_name']);
-			});
-		});
-	};
 	return (
 		<div>
 			<h1>Weather App</h1>
-			<form onSubmit={handleSubmit}>
+			<form onSubmit={getWeatherByCity}>
 				<label>
 					City :
 					<input type="text" value={city} onChange={(e) => setCity(e.target.value)} />
 				</label>
-				<input type="submit" value="Submit" />
+				<input type="submit" value="Check Weather" />
 			</form>
+			{location ? <Result /> : null}
 		</div>
 	);
 }
